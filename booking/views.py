7feffe74b7286c50bbedb8d7c7320713d3d0ad22
tamirgen <tamirgenkin@gmail.com>
2022-02-24 13:57:08 +0000

    Add RoomDetailsView class

--- booking/views.py
+++ booking/views.py
@@ -1,17 +1,66 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, View
 from .models import Room, Booking
 from .forms import AvailabilityForm
 from booking.booking_functions.availability import check_availability
 
-
 # Create your views here.
 
-class RoomList(ListView):
+
+class RoomListView(ListView):
     model = Room
+
 
 class BookingList(ListView):
     model = Booking
+
+
+class RoomDetailView(View):
+    def get(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
+        form = AvailabilityForm()
+        room_list = Room.objects.filter(category=category)
+
+        print(category)
+
+        if len(room_list) > 0:
+            room = room_list[0]
+            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
+            context = {
+                'room_category': room_category,
+                'form': form,
+            }
+            return render(request, 'room_detail_view.html', context)
+        else:
+            return HttpResponse('Category does not exist')
+        
+
+    def post(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
+        room_list = Room.objects.filter(category=category)
+        form = AvailabilityForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+        available_rooms = []
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
+
+        if len(available_rooms) > 0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user=self.request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out']
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('All of this category of rooms are booked!! Try another one')
+
 
 class BookingView(FormView):
     form_class = AvailabilityForm
@@ -19,22 +68,21 @@ class BookingView(FormView):
 
     def form_valid(self, form):
         data = form.cleaned_data
-        available_rooms =[]
         room_list = Room.objects.filter(category=data['room_category'])
+        available_rooms = []
         for room in room_list:
             if check_availability(room, data['check_in'], data['check_out']):
                 available_rooms.append(room)
 
-        if len(available_rooms)>0:
+        if len(available_rooms) > 0:
             room = available_rooms[0]
             booking = Booking.objects.create(
-                user = self.request.user,
-                room = room,
-                check_in = data['check_in'],
-                check_out =  data['check_out'],
+                user=self.request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out']
             )
             booking.save()
             return HttpResponse(booking)
-
         else:
-            return HttpResponse ('This category is full, please choose another category')
+            return HttpResponse('All of this category of rooms are booked!! Try another one')
