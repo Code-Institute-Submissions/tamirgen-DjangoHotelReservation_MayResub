6ed3c51808aa2e7ab15512440994ade230c1c10e
tamirgen <tamirgenkin@gmail.com>
2022-03-25 08:51:51 +0000

    Change class RoomDetailView class so user get an error massage if he is not logged in and render the page to Booking_List page

--- booking/models.py
+++ booking/models.py
@@ -2,21 +2,18 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse_lazy
 
+class Room_Categories(models.Model):
+    name=models.CharField(max_length=10)
+    description=models.CharField(max_length=1000)
 
 class Room(models.Model):
-    ROOM_CATEGORIES = (
-        ('SIN', 'SINGLE'),
-        ('DBL', 'DOUBLE'),
-        ('QUE', 'QUEEN'),
-        ('KIN', 'KING'),
-    )
     number = models.IntegerField()
-    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    category = models.ForeignKey(Room_Categories, on_delete=models.CASCADE)
     beds = models.IntegerField()
     capacity = models.IntegerField()
 
     def __str__(self):
-        return f'{self.number}. {self.category} with {self.beds} beds for {self.capacity} people'
+        return f'{self.number}. {self.category.name} with {self.beds} beds for {self.capacity} people'
 
 class Booking(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -27,10 +24,8 @@ class Booking(models.Model):
     def __str__(self):
         return f'{self.user} booked {self.room} from {self.check_in} to {self.check_out}'
 
-    def get_room_category(self):
-        room_categories = dict(self.room.ROOM_CATEGORIES)
-        room_category = room_categories.get(self.room.category)
-        return room_category
-
     def get_cancel_booking_url(self):
         return reverse_lazy('booking:CancelBookingView', args=[self.pk, ])
+
+    def get_room_category(self):
+        return f'{self.room.category.name}'
